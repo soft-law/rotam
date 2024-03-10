ab081f36c00b43b797e3b840c301d7e475b0405d
nagrarohit <rohit.nagra7861@gmail.com>
2024-03-10 20:43:34 +0530

    team section updated

--- frontend/comps/team.tsx
+++ frontend/comps/team.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 const TeamSection = () => {
   // Define team members
   const teamMembers = [
-    { name: "Mario Andrade", role: "Founder, CEO" },
-    { name: "Nagra Rohit", role: "Smart-Contract Developer" },
-    { name: "Pat Sinma", role: "Product-Designer" },
-    { name: "Ganesh", role: "Back-End Developer" },
-
-    // Add more team members as needed
+    { name: "Mario Andrade", role: "Founder, CEO", image: "/mario.png" },
+    {
+      name: "Nagra Rohit",
+      role: "Smart-Contract Developer",
+      image: "/rohit1.png",
+    },
+    { name: "Pat Sinma", role: "Product-Designer", image: "/pat.png" },
+    { name: "Ganesh Oli", role: "Back-End Developer", image: "/ganesh.png" },
+    // { name: "Rishad", role: "Front-End Developer", image: "/rishad.jpg" },
   ];
 
   // Calculate the alignment based on the number of team members
@@ -62,18 +66,48 @@ const TeamSection = () => {
               maxWidth: "300px", // Limit width to avoid overflow on smaller screens
               paddingTop: "8px", // Add some padding to separate the line from the text
               textAlign: "left", // Center align the text
+              borderTop: "3px solid #8975EA",
             }}
           >
+            <div
+              style={{
+                width: "177px",
+                height: "177px",
+                borderRadius: "50%",
+                overflow: "hidden",
+                marginBottom: "10px",
+                marginTop: "10px",
+              }}
+            >
+              {member.image && (
+                <Image
+                  width={250}
+                  height={250}
+                  style={{ objectFit: "cover" }}
+                  alt={member.name}
+                  src={member.image}
+                />
+              )}
+            </div>
             <h2
               style={{
                 fontWeight: "700",
-                borderTop: "1px solid #8975EA", // Add a border top to create the line
+                fontSize: "39px",
+
                 paddingTop: "8px", // Add some space above the name
               }}
             >
               {member.name}
             </h2>
-            <p>{member.role}</p>
+
+            <p
+              style={{
+                fontWeight: "450",
+                fontSize: "15px",
+              }}
+            >
+              {member.role}
+            </p>
           </div>
         ))}
       </div>
